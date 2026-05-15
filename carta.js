c92d5ad53c05cabeb8045cf8bc83a552a22ad40b
Luca Famozo <123339054+lucafamozo@users.noreply.github.com>
2026-05-15 19:39:20 -0300

    puntos 2 y 3 (y 10)

---
+++ carta.js
@@ -0,0 +1,51 @@
+class Carta {
+    code;
+    value;
+    suit;
+    imagen;
+     constructor (code, value, suit, imagen) {
+        if (!code || !value || !suit || !imagen) {
+            throw new Error("Los datos son inválidos")
+        }
+
+        this.code = code;
+        this.value = value;
+        this.suite = suit;
+        this.imagen = imagen;
+     }
+
+     toJsonString() {
+        return JSON.stringify(this);
+    }
+
+    static  createFromJsonString(json) {
+        const datos = JSON.parse(json);
+        return new Carta(datos.code, datos.value, datos.suit, datos.imagen); 
+    }
+    createHtmlElement() {
+        const div = document.createElement("div");
+        const img = document.createElement("img");
+        const h3 = document.createElement("h3");
+        const p = document.createElement("p");
+
+        const link = document.createElement("a");
+        link.href = this.imagen;
+        link.target = "_blank";
+        link.appendChild(img);
+
+        img.src = this.imagen;
+        h3.textContent = this.code;
+        p.textContent = this.suit + " - " + this.value;
+
+        const boton = document.createElement("button");
+        boton.textContent = "guardar";
+        boton.addEventListener("click", () => Carta.guardarCarta(this));
+
+        div.appendChild(link);
+        div.appendChild(h3);
+        div.appendChild(p);
+        div.appendChild(boton);
+
+        return div;
+    }
+}
