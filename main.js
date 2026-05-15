e5ec5df147d86f3df2edda1a134b8c111546ce40
Luca Famozo <123339054+lucafamozo@users.noreply.github.com>
2026-05-15 20:07:48 -0300

    punto 5

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+let historial = [];
+let paginaActual = [];
+
 async function cargarCartas() {
   const contenedor = document.getElementById("cartas");
   contenedor.innerHTML = "";
 
   const respuesta = await fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=6");
   const datos = await respuesta.json();
+  paginaActual = datos.cards;
 
-  for (const cartaDatos of datos.cards) {
+  for (const cartaDatos of paginaActual) {
     const carta = new Carta(
       cartaDatos.code,
       cartaDatos.value,
@@ -17,4 +21,29 @@ async function cargarCartas() {
   }
 }
 
+async function paginaSiguiente() {
+  historial.push(paginaActual);
+  await cargarCartas();
+}
+
+async function paginaAnterior() {
+  if (historial.length > 0) {
+    const contenedor = document.getElementById("cartas");
+    contenedor.innerHTML = "";
+    paginaActual = historial.pop();
+
+    for (const cartaDatos of paginaActual) {
+      const carta = new Carta(
+        cartaDatos.code,
+        cartaDatos.value,
+        cartaDatos.suit,
+        cartaDatos.image,
+      );
+      contenedor.appendChild(carta.createHtmlElement());
+    }
+  }
+}
+
 window.addEventListener("load", cargarCartas);
+document.getElementById("siguiente").addEventListener("click", paginaSiguiente);
+document.getElementById("anterior").addEventListener("click", paginaAnterior);
