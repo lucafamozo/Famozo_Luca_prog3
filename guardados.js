8f50a6ea5643489f6db3d9978a9901ac0ebd5d23
Luca Famozo <123339054+lucafamozo@users.noreply.github.com>
2026-05-15 21:08:19 -0300

    punto 9

--- guardados.js
+++ guardados.js
@@ -1,8 +1,10 @@
 //localStorage.clear()  <---- lo estuve usando para borrar el historial de cartas guardadas, lo dejo para pruebas
 
-window.addEventListener("load", () => {
+let guardadas = [];
+
+function mostrarCartas() {
   const contenedor = document.getElementById("cartas");
-  const guardadas = JSON.parse(localStorage.getItem("cartas") || "[]");
+  contenedor.innerHTML = "";
 
   for (const cartaDatos of guardadas) {
     const carta = new Carta(
@@ -11,7 +13,24 @@ window.addEventListener("load", () => {
       cartaDatos.suit,
       cartaDatos.imagen,
     );
-    const elemento = carta.createHtmlElement();
-    contenedor.appendChild(elemento);
+    contenedor.appendChild(carta.createHtmlElement());
   }
+}
+
+window.addEventListener("load", () => {
+  guardadas = JSON.parse(localStorage.getItem("cartas") || "[]");
+  mostrarCartas();
+
+  document.getElementById("ordenarSuit").addEventListener("click", () => {
+    guardadas.sort((a, b) => a.suit.localeCompare(b.suit));
+    mostrarCartas();
+  });
+
+
+  // uso un array "orden" porque el valor de las cartas de póker no son en orden alfabético, ni tampoco completamente numérico
+  document.getElementById("ordenarValue").addEventListener("click", () => {
+    const orden = ["ACE", "2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING"];
+    guardadas.sort((a, b) => orden.indexOf(a.value) - orden.indexOf(b.value));
+    mostrarCartas();
+  });
 });
